53db1b46dc8a9b44ecc2942668f27c38be6f77a0
alejandrodelcastillo <130861049+alejandrodelcastillo@users.noreply.github.com>
2023-05-23 23:49:18 -0300

    tarea correspondiente a la Clase 12
    actividades de la Clase 12

---
+++ clase12/js/javascript.js
@@ -0,0 +1,14 @@
+function saludo(){
+
+}
+var nombre = prompt("Hola, cual es tu nombre?");
+alert("Bienvenido/a"+" "+nombre);
+var nombre = prompt("Hola, cual es tu nombre?");
+alert("Intoduzca su nombre"+" "+nombre);
+
+
+
+
+                
+        
+        
